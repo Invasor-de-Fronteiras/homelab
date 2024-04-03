d59ef94b87b778ab25612358246b24dfb3d4a42a
Yslan Ramos <yslanramos.r@gmail.com>
2024-04-03 00:52:50 -0300

    chore: add argocd application to start up

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -7,3 +7,16 @@ resource "helm_release" "argocd" {
   version          = "6.7.8"
   create_namespace = true
 }
+
+resource "helm_release" "argocd-apps" {
+  name             = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "argocd"
+  version          = "2.0.0"
+  create_namespace = true
+
+  values = [
+    file("values/argocd.yaml")
+  ]
+}
